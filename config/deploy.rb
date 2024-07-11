baf85595f258233a667305fc1b6781f4e3b7f02b
same06 <typewreter@yahoo.co.jp>
2024-07-11 10:18:43 +0900

    修正

---
+++ config/deploy.rb
@@ -0,0 +1,46 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.19.1"
+
+#capistranoの設定
+set :application, "blog_app" #ec2に置かれるアプリケーション名
+set :repo_url, "https://github.com/same6318/same23.git" #必ずhttps
+set :bundle_without, %w{test}.join(':') #gemのインストールをする設定（テスト環境以外も）
+
+set :rbenv_version, '3.3.0'
+
+append :linked_files, 'config/secrets.yml'
+
+set :branch, 'main'
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", 'config/master.key'
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
